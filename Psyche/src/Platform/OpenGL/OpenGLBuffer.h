b908e98fe563fb8c04eddae9fe0b60132363cc1b
LiangRVR <liangvillarrubia95@gmail.com>
2024-07-02 15:02:14 -0400

    chore: Update OpenGLBuffer to include buffer layout in Psyche namespace

--- Psyche/src/Platform/OpenGL/OpenGLBuffer.h
+++ Psyche/src/Platform/OpenGL/OpenGLBuffer.h
@@ -1,11 +1,11 @@
 #pragma once
 #include "Psyche/Renderer/Buffer.h"
 
-namespace Psyche{
+namespace Psyche {
     ///////////////////////////////////////////////////////////////////////////
     // Vertex Buffer //////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
-    class OpenGLVertexBuffer : public VertexBuffer{
+    class OpenGLVertexBuffer : public VertexBuffer {
       public:
         OpenGLVertexBuffer(float *vertices, uint32_t size);
         virtual ~OpenGLVertexBuffer();
@@ -13,8 +13,12 @@ namespace Psyche{
         virtual void Bind() const override;
         virtual void Unbind() const override;
 
+        virtual const BufferLayout &GetLayout() const override { return m_Layout; };
+        virtual void SetLayout(const BufferLayout &layout) override { m_Layout = layout; };
+
       private:
         uint32_t m_RendererID;
+        BufferLayout m_Layout;
     };
 
     ///////////////////////////////////////////////////////////////////////////
@@ -25,7 +29,7 @@ namespace Psyche{
         OpenGLIndexBuffer(uint32_t *indices, uint32_t count);
         virtual ~OpenGLIndexBuffer();
 
-        virtual uint32_t GetCount() const override { return m_Count;}
+        virtual uint32_t GetCount() const override { return m_Count; }
 
         virtual void Bind() const override;
         virtual void Unbind() const override;
@@ -34,4 +38,4 @@ namespace Psyche{
         uint32_t m_RendererID;
         uint32_t m_Count;
     };
-}
+} // namespace Psyche
--- Psyche/src/Psyche/Application.cpp
+++ Psyche/src/Psyche/Application.cpp
@@ -12,6 +12,29 @@ namespace Psyche {
 
     Application *Application::s_Instance = nullptr;
 
+    static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type) {
+        switch (type) {
+        case ShaderDataType::Float:
+        case ShaderDataType::Float2:
+        case ShaderDataType::Float3:
+        case ShaderDataType::Float4:
+        case ShaderDataType::Mat3:
+        case ShaderDataType::Mat4:   return GL_FLOAT;
+
+        case ShaderDataType::Int:
+        case ShaderDataType::Int2:
+        case ShaderDataType::Int3:
+        case ShaderDataType::Int4: return GL_INT;
+
+        case ShaderDataType::Bool: return GL_BOOL;
+
+        case ShaderDataType::None: break;
+        }
+
+        PSC_CORE_ASSERT(false, "Unknown ShaderDataType!");
+        return 0;
+    }
+
     Application::Application() {
         PSC_CORE_ASSERT(!s_Instance, "Application already exists!");
         s_Instance = this;
@@ -25,12 +48,34 @@ namespace Psyche {
         glGenVertexArrays(1, &m_VertexArray);
         glBindVertexArray(m_VertexArray);
 
-        float vertices[3 * 3] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+        // clang-format off
+        float vertices[3 * 7] = {-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+                                  0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+                                  0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f};
+        // clang-format on
 
         m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
+        {
+            BufferLayout layout = {
+                {ShaderDataType::Float3, "a_Position"},
+                {ShaderDataType::Float4,    "a_Color"}
+            };
+            m_VertexBuffer->SetLayout(layout);
+        }
 
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+        const auto &layout = m_VertexBuffer->GetLayout();
+
+        uint32_t index = 0;
+        for (const auto &element : layout) {
+            glEnableVertexAttribArray(index);
+            glVertexAttribPointer(index,
+                                  element.GetComponentCount(),
+                                  ShaderDataTypeToOpenGLBaseType(element.Type),
+                                  element.Normalized ? GL_TRUE : GL_FALSE,
+                                  layout.GetStride(),
+                                  reinterpret_cast<const void *>(static_cast<std::uintptr_t>(element.Offset)));
+            index++;
+        }
 
         uint32_t indices[3] = {0, 1, 2};
 
@@ -40,11 +85,14 @@ namespace Psyche {
             #version 330 core
 
             layout(location = 0) in vec3 a_Position;
+            layout(location = 1) in vec4 a_Color;
 
             out vec3 v_Position;
+            out vec4 v_Color;
 
             void main() {
                 v_Position = a_Position + 0.5;
+                v_Color = a_Color;
                 gl_Position = vec4(a_Position + 0.5, 1.0);
             }
         )";
@@ -55,15 +103,16 @@ namespace Psyche {
             layout(location = 0) out vec4 color;
 
             in vec3 v_Position;
+            in vec4 v_Color;
 
             void main() {
                 color = vec4(v_Position*0.5 + 0.5, 1.0);
+                color = v_Color;
             }
         )";
 
         m_Shader.reset(new Shader(vertexSrc, fragmentSrc));
     }
-
 
     void Application::PushLayer(Layer *layer) { m_LayerStack.PushLayer(layer); }
 
