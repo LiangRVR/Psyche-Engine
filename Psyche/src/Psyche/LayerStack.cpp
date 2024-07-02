81bf8fb9907bc41b2bee064f03ecc030b3d6bb1d
LiangRVR <liangvillarrubia95@gmail.com>
2024-07-02 19:39:29 -0400

    chore: Refactor LayerStack PopLayer and PopOverlay methods. LayerStack only need to search for the layer to be pop in its correspond location

--- Psyche/src/Psyche/LayerStack.cpp
+++ Psyche/src/Psyche/LayerStack.cpp
@@ -23,7 +23,7 @@ namespace Psyche {
     }
 
     void LayerStack::PopLayer(Layer *layer) {
-        auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
+        auto it = std::find(m_Layers.begin(), m_Layers.begin() + m_LayerInsertIndex, layer);
         if (it != m_Layers.end()) {
             layer->OnDetach();
             m_Layers.erase(it);
@@ -32,7 +32,7 @@ namespace Psyche {
     }
 
     void LayerStack::PopOverlay(Layer *overlay) {
-        auto it = std::find(m_Layers.begin(), m_Layers.end(), overlay);
+        auto it = std::find(m_Layers.begin() + m_LayerInsertIndex, m_Layers.end(), overlay);
         if (it != m_Layers.end()) {
             overlay->OnDetach();
             m_Layers.erase(it);
